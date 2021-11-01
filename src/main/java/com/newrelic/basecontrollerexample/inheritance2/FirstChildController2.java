51553819c38f4dee4b53f76fb0e3a2857e91c81d
Todd W Crone <twcrone@gmail.com>
2021-11-01 10:51:16 -0400

    Add logging configuration

--- src/main/java/com/newrelic/basecontrollerexample/inheritance2/FirstChildController2.java
+++ src/main/java/com/newrelic/basecontrollerexample/inheritance2/FirstChildController2.java
@@ -1,5 +1,7 @@
 package com.newrelic.basecontrollerexample.inheritance2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/inherited2/v1")
 public class FirstChildController2 extends ParentController2 {
+    Logger logger = LoggerFactory.getLogger(FirstChildController2.class);
+
+    public FirstChildController2() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+    }
+
     @PostMapping
     public String doStuff() {
-        // do things
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return "First child done doing stuff";
     }
 }
