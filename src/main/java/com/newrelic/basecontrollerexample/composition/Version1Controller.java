db486681d2e5fe1d6a0328cf776f45c2cbc1830d
Todd W Crone <twcrone@gmail.com>
2021-11-02 14:13:36 -0400

    Fix the config and ignore log files

--- src/main/java/com/newrelic/basecontrollerexample/composition/Version1Controller.java
+++ src/main/java/com/newrelic/basecontrollerexample/composition/Version1Controller.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/composed/v1")
 public class Version1Controller {
+
+    private final Logger logger = Logger.getLogger(Version1Controller.class.getName());
 
     private final GetStuffAndDoThingsService service;
 
@@ -18,6 +23,11 @@ public class Version1Controller {
     @GetMapping("/method")
     public String getStuff() {
         String stuff = service.getStuffAndDoThings();
+        logger.log(Level.FINEST, "A FINEST Message");
+        logger.log(Level.FINE, "A FINE Message");
+        logger.info("An INFO Message");
+        logger.log(Level.WARNING, "A WARN Message");
+        logger.log(Level.SEVERE, "An ERROR Message");
         return stuff;
     }
 
--- src/main/java/com/newrelic/basecontrollerexample/inheritance2/FirstChildController2.java
+++ src/main/java/com/newrelic/basecontrollerexample/inheritance2/FirstChildController2.java
@@ -1,32 +1,32 @@
 package com.newrelic.basecontrollerexample.inheritance2;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/inherited2/v1")
 public class FirstChildController2 extends ParentController2 {
-    Logger logger = LoggerFactory.getLogger(FirstChildController2.class);
+    Logger logger = Logger.getLogger(FirstChildController2.class.getName());
 
     public FirstChildController2() {
-        logger.trace("A TRACE Message");
-        logger.debug("A DEBUG Message");
+        logger.log(Level.FINEST, "A FINEST Message");
+        logger.log(Level.FINE, "A FINE Message");
         logger.info("An INFO Message");
-        logger.warn("A WARN Message");
-        logger.error("An ERROR Message");
-
+        logger.log(Level.WARNING, "A WARN Message");
+        logger.log(Level.SEVERE, "An ERROR Message");
     }
 
     @PostMapping
     public String doStuff() {
-        logger.trace("A TRACE Message");
-        logger.debug("A DEBUG Message");
+        logger.log(Level.FINEST, "A FINEST Message");
+        logger.log(Level.FINE, "A FINE Message");
         logger.info("An INFO Message");
-        logger.warn("A WARN Message");
-        logger.error("An ERROR Message");
+        logger.log(Level.WARNING, "A WARN Message");
+        logger.log(Level.SEVERE, "An ERROR Message");
         return "First child done doing stuff";
     }
 }
